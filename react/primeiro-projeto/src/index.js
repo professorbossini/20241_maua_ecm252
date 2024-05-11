ab0827299a7e070520a48e1a13c847fa98f81bae
Rodrigo Bossini <professorbossini@gmail.com>
2024-05-11 12:21:26 -0300

    feat(ap03): encerra a apostila
    - exercicio 03 proposto e resolvido em aula

--- react/primeiro-projeto/src/index.js
+++ react/primeiro-projeto/src/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import './styles.css'
 //classes ou funções
 
 const App = () => {
-  return <div>Meu primeiro componente React</div>
+
+  const estilosDoBotao = { marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8 }
+
+  const textoDoRotulo = 'Nome:'
+
+  const obterTextoDoBotao = () => {
+    return 'Enviar'
+  }
+
+  const clicou = () => alert('clicou')
+
+  return (
+    <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+      <label className='rotulo' htmlFor='nome' style={{display: 'block', marginBottom: 4}}>{textoDoRotulo}</label>
+      <input type='text' id='nome' style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
+      <button 
+        style={estilosDoBotao}
+        onClick={clicou}>
+        {obterTextoDoBotao()}
+      </button>
+    </div>
+  )
 }
 
 ReactDOM.render(
